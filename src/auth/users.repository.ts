ef0c939d04115964b442af9b9d0181d820c4f6eb
Ofir-Roz <ofir19923@gmail.com>
2025-08-15 16:28:41 +0300

    feat: implement authentication module with user signup functionality

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,29 @@
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+
+@Injectable()
+export class UsersRepository {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>
+    ) {}
+
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const user = this.usersRepository.create({ username, password });
+        try {
+            await this.usersRepository.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            }
+            else {
+                throw new InternalServerErrorException('Failed to create user');
+            }
+        }
+    }
+}
